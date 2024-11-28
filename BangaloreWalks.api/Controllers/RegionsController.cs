5f3da330df57af58a9c6b25fc95113f28d753858
MSI-1 <MSI-1@MSI>
2024-11-29 00:08:58 +0530

    Add project files.

---
+++ BangaloreWalks.api/Controllers/RegionsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Entity;
+using BangaloreWalks.api.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BangaloreWalks.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly BangaloreWalksDbContext dbContext;
+
+        public RegionsController(BangaloreWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IActionResult GetAll()
+        {
+
+             var Regions = dbContext.Regions.ToList();
+            return Ok(Regions);
+;        }
+
+    }
+}
---
+++ BangaloreWalks.api/Data/BangaloreWalksDbContext.cs
@@ -0,0 +1,33 @@
+﻿using BangaloreWalks.api.Models.Domain;
+using BangaloreWalks.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BangaloreWalks.api.Data
+{
+    public class BangaloreWalksDbContext :DbContext
+    {
+        //public BangaloreWalksDbContext(DbContextOptions dbContextOptions) :base(dbContextOptions) { 
+
+
+
+
+        //}
+
+        public BangaloreWalksDbContext(DbContextOptions<BangaloreWalksDbContext> options) : base(options)
+        {
+
+        }
+
+        
+
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+
+
+
+
+    }
+}
